d875988e010c0706b2b3fa2ebbe15980f7888c9c
Ben Janis <btjanis@mitre.org>
2021-06-02 11:05:04 -0400

    Cleanup

--- hints/hint2.py
+++ hints/hint2.py
@@ -1,9 +1,49 @@
 '''
-Run this cell again to check out the man page for strcmp.
+Check out the below man page for strcmp.
 
 What are the possible return values?
 
 What impacts could different return values have on the structure of the power trace?
-'''
 
-!man strcmp
+
+
+STRCMP(3)                BSD Library Functions Manual                STRCMP(3)
+
+NAME
+     strcmp, strncmp -- compare strings
+
+LIBRARY
+     Standard C Library (libc, -lc)
+
+SYNOPSIS
+     #include <string.h>
+
+     int
+     strcmp(const char *s1, const char *s2);
+
+     int
+     strncmp(const char *s1, const char *s2, size_t n);
+
+DESCRIPTION
+     The strcmp() and strncmp() functions lexicographically compare the null-
+     terminated strings s1 and s2.
+
+     The strncmp() function compares not more than n characters.  Because
+     strncmp() is designed for comparing strings rather than binary data,
+     characters that appear after a `\0' character are not compared.
+
+RETURN VALUES
+     The strcmp() and strncmp() functions return an integer greater than,
+     equal to, or less than 0, according as the string s1 is greater than,
+     equal to, or less than the string s2.  The comparison is done using
+     unsigned characters, so that `\200' is greater than `\0'.
+
+SEE ALSO
+     bcmp(3), memcmp(3), strcasecmp(3), strcoll(3), strxfrm(3), wcscmp(3)
+
+STANDARDS
+     The strcmp() and strncmp() functions conform to ISO/IEC 9899:1990
+     (``ISO C90'').
+
+BSD                            October 11, 2001                            BSD
+'''
